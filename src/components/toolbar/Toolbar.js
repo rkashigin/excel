ea6634ac89da3c9396cfe68425585e82b4cebe0d
Roman Kashigin <r.kashigin@yandex.ru>
2021-02-10 18:13:38 +0300

    Toolbar component refactored. Toolbar icons now depend on state (active / not active)

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,52 +1,42 @@
-import {ExcelComponent} from "@core/ExcelComponent";
+import {ExcelStateComponent} from "@core/ExcelStateComponent";
+import {createToolbar} from "@/components/toolbar/toolbar.template";
+import {$} from "@core/Dom";
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
   static className = "excel__toolbar";
 
   constructor($root, options) {
     super($root, {
       name: "Toolbar",
+      listeners: ["click"],
       ...options
     });
   }
 
+  prepare() {
+    const initialState = {
+      textAlign: "left",
+      fontWeight: "normal",
+      textDecoration: "none",
+      fontStyle: "normal"
+    };
+
+    this.initState(initialState);
+  }
+
+  get template() {
+    return createToolbar(this.state);
+  }
+
   toHTML() {
-    return `
-      <div class="button">
-          <span class="material-icons">
-            format_align_left
-          </span>
-      </div>
+    return this.template;
+  }
 
-      <div class="button">
-          <span class="material-icons">
-            format_align_center
-          </span>
-      </div>
-
-      <div class="button">
-          <span class="material-icons">
-            format_align_right
-          </span>
-      </div>
-
-      <div class="button">
-          <span class="material-icons">
-            format_bold
-          </span>
-      </div>
-
-      <div class="button">
-          <span class="material-icons">
-            format_italic
-          </span>
-      </div>
-
-      <div class="button">
-          <span class="material-icons">
-            format_underline
-          </span>
-      </div>
-    `;
+  onClick(event) {
+    const $target = $(event.target);
+    if ($target.data.type === "button") {
+      const value = JSON.parse($target.data.value);
+      this.setState(value);
+    }
   }
 }
