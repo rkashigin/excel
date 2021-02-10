04caf2f7e8c5f8c61c2a6c18213ac4a2f769cbad
Roman Kashigin <r.kashigin@yandex.ru>
2021-02-10 12:16:38 +0300

    Action for table resizing added. Now width of a column will be saved in state after being resized

--- src/components/table/table.resize.js
+++ src/components/table/table.resize.js
@@ -1,56 +1,63 @@
 import {$} from "@core/Dom";
 
 export function resizeHandler($root, event) {
-  const $resizer = $(event.target);
-  const $parent = $resizer.closest('[data-type="resizable"]');
-  const coords = $parent.getCoords();
-  const type = event.target.dataset.resize;
+  return new Promise(resolve => {
+    const $resizer = $(event.target);
+    const $parent = $resizer.closest('[data-type="resizable"]');
+    const coords = $parent.getCoords();
+    const type = event.target.dataset.resize;
 
-  $resizer.css({
-    opacity: 1,
-  });
+    $resizer.css({
+      opacity: 1,
+    });
 
-  let value;
+    let value;
 
-  document.onmousemove = e => {
-    if (type === "col") {
-      const delta = e.pageX - coords.right;
-      value = coords.width + delta;
-      $resizer.css({
-        right: -delta + "px",
-        height: $root.$el.scrollHeight - 2 + "px"
-      });
-    } else {
-      const delta = e.pageY - coords.bottom;
-      value = coords.height + delta;
-      $resizer.css({
-        bottom: -delta + "px",
-        width: $root.$el.scrollWidth - 2 + "px"
-      });
-    }
-  };
+    document.onmousemove = e => {
+      if (type === "col") {
+        const delta = e.pageX - coords.right;
+        value = coords.width + delta;
+        $resizer.css({
+          right: -delta + "px",
+          height: $root.$el.scrollHeight - 2 + "px"
+        });
+      } else {
+        const delta = e.pageY - coords.bottom;
+        value = coords.height + delta;
+        $resizer.css({
+          bottom: -delta + "px",
+          width: $root.$el.scrollWidth - 2 + "px"
+        });
+      }
+    };
 
-  document.onmouseup = () => {
-    document.onmousemove = null;
-    document.onmouseup = null;
+    document.onmouseup = () => {
+      document.onmousemove = null;
+      document.onmouseup = null;
 
-    $resizer.removeCss([
-      'opacity',
-      'right',
-      'bottom',
-      'height',
-      'width'
-    ]);
+      $resizer.removeCss([
+        'opacity',
+        'right',
+        'bottom',
+        'height',
+        'width'
+      ]);
 
-    if (type === "col") {
-      $root.findAll(`[data-col="${$parent.data.col}"]`)
-          .forEach(cell => {
-            $(cell).css({
-              width: value + 'px'
+      if (type === "col") {
+        $root.findAll(`[data-col="${$parent.data.col}"]`)
+            .forEach(cell => {
+              $(cell).css({
+                width: value + 'px'
+              });
             });
-          });
-    } else {
-      $parent.css({height: value + "px"});
-    }
-  };
+      } else {
+        $parent.css({height: value + "px"});
+      }
+
+      resolve({
+        value,
+        id: type === "col" ? $parent.data.col : null
+      });
+    };
+  });
 }
